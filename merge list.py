ff3f382180560cec91d547fe5912dffd39805b7f
Sanjana-Singh28 <sanjusingh246809@gmail.com>
2026-08-08 13:18:36 +0530

    merged two list

---
+++ merge list.py
@@ -0,0 +1,11 @@
+# n=[1,2,3,4,"sanjana"]
+# d=[4,6,5,"sujit"]
+# c=n+d
+# print("the merged list is ",c)
+
+#merged list by taking user input
+
+list1=["sanjana","singh","sujit"]
+list2=["sujit","sonam","sujal"]
+merge=list1+list2
+print("merged list is :",merge)
